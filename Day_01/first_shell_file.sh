cbf2765a9c5dfe1a34562257d23b3a4077b6c48a
Vivek <mailvivek72@gmail.com>
2024-07-09 12:51:31 +0530

    updated code

--- Day_01/first_shell_file.sh
+++ Day_01/first_shell_file.sh
@@ -4,4 +4,6 @@ systemctl start httpd
 wget https://www.free-css.com/assets/files/free-css-templates/download/page296/oxer.zip
 unzip oxer.zip
 cp -R oxer-html/* /var/www/html/
+systemctl restart httpd
+systemctl enable httpd
 
