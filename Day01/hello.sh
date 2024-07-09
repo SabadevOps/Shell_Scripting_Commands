b536d0075e70d36e9050bf9320028a4938ee2915
Vivek <mailvivek72@gmail.com>
2024-07-09 12:32:27 +0530

    this is my first shell scripting file

---
+++ Day01/hello.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#This is my first shell command file
+
+echo "hi"
+
+echo "hello"
+
+echo "how are you"
+
+
---
+++ Day_01/first_shell_file.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+echo "me: hi friends"
+
+echo "hello"
+
+echo "me: how are you"
