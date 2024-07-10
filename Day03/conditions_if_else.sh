f500c2454da7341a7ce80153e063f0d649852c81
Vivek <mailvivek72@gmail.com>
2024-07-10 21:15:03 +0530

    updated day 04

---
+++ Day03/conditions_if_else.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+<< disclaimer
+This is just for educational purpose
+disclaimer
+
+read -p "Jetha ne mud k kise dekha: " bandi
+
+if [[ $bandi == "daya bhabhi" ]]
+then
+	echo "Jetha is loyal"
+
+else
+	echo "Jetha is not loyal"
+fi
